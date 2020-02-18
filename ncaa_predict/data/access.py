3e9bd2b1b85b54210a51f940948e9a52d9a5a74a
rich <richard.w.harris@mac.com>
2020-02-18 03:10:37 +0000

    added column comments to data access class methods

--- ncaa_predict/data/access.py
+++ ncaa_predict/data/access.py
@@ -22,48 +22,63 @@ class DataAccess():
         return _df
 
     def cities_df(self) -> pd.DataFrame:
+        # CityID, City, State
         return self._read_stage_1_file(name='Cities.csv')
 
     def conferences_df(self) -> pd.DataFrame:
+        # ConfAbbrev, Description
         return self._read_stage_1_file(name='Conferences.csv')
 
     def conferences_tourney_games_df(self) -> pd.DataFrame:
+        # Season, ConfAbbrev, DayNum, WTeamID, LTeamID
         return self._read_stage_1_file(name=self.prefix + 'ConferenceTourneyGames.csv')
 
     def game_cities_df(self) -> pd.DataFrame:
+        # Season, DayNum, WTeamID, LTeamID, CRType, CityID
         return self._read_stage_1_file(name=self.prefix + 'GameCities.csv')
 
     def tourney_compact_results_df(self) -> pd.DataFrame:
+        # Season, DayNum, WTeamID, WScore, LTeamID, LScore, WLoc,NumOT
         return self._read_stage_1_file(name=self.prefix + 'NCAATourneyCompactResults.csv')
 
     def tourney_detailed_results_df(self) -> pd.DataFrame:
+        # Season, DayNum, WTeamID, WScore, LTeamID, LScore, WLoc, NumOT, WFGM, WFGA, WFGM3, WFGA3, WFTM, WFTA, WOR, WDR, WAst, WTO, WStl, WBlk, WPF, LFGM, LFGA, LFGM3, LFGA3, LFTM, LFTA, LOR, LDR, LAst, LTO, LStl, LBlk, LPF
         return self._read_stage_1_file(name=self.prefix + 'NCAATourneyDetailedResults.csv')
 
     def tourney_seeds_df(self) -> pd.DataFrame:
+        # Season, Seed, TeamID
         return self._read_stage_1_file(name=self.prefix + 'NCAATourneySeeds.csv')
 
     def tourney_slots_df(self) -> pd.DataFrame:
+        # Slot, StrongSeed, WeakSeed
         return self._read_stage_1_file(name=self.prefix + 'NCAATourneySlots.csv')
 
     def regular_season_compact_results_df(self) -> pd.DataFrame:
+        # Season, DayNum, WTeamID, WScore, LTeamID, LScore, WLoc, NumOT
         return self._read_stage_1_file(name=self.prefix + 'RegularSeasonCompactResults.csv')
 
     def regular_season_detailed_results_df(self) -> pd.DataFrame:
+        # Season, DayNum, WTeamID, WScore, LTeamID, LScore, WLoc, NumOT, WFGM, WFGA, WFGM3, WFGA3, WFTM, WFTA, WOR, WDR, WAst, WTO, WStl, WBlk, WPF, LFGM, LFGA, LFGM3, LFGA3, LFTM, LFTA, LOR, LDR, LAst, LTO, LStl, LBlk, LPF
         return self._read_stage_1_file(name=self.prefix + 'RegularSeasonDetailedResults.csv')
 
     def seasons_df(self) -> pd.DataFrame:
+        # Season, DayZero, RegionW, RegionX, RegionY, RegionZ
         return self._read_stage_1_file(name=self.prefix + 'Seasons.csv')
 
     def team_conferences_df(self) -> pd.DataFrame:
+        # Season, TeamID, ConfAbbrev
         return self._read_stage_1_file(name=self.prefix + 'TeamConferences.csv')
 
     def teams_df(self) -> pd.DataFrame:
+        # TeamID, TeamName
         return self._read_stage_1_file(name=self.prefix + 'Teams.csv')
 
     def events_df(self, season: int) -> pd.DataFrame:
+        # EventID, Season, DayNum, WTeamID, LTeamID, WFinalScore, LFinalScore, WCurrentScore, LCurrentScore, ElapsedSeconds, EventTeamID, EventPlayerID, EventType, EventSubType, X, Y, Area
         return self._read(name=f'{self.prefix}Events{season}.csv')
 
     def players_df(self) -> pd.DataFrame:
+        # PlayerID, LastName, FirstName, TeamID
         return self._read(name=f'{self.prefix}Players.csv')
 
 
