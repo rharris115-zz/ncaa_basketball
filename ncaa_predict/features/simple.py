be3ef84f9ccb6290635b1de056f6d84d31038bee
rich <richard.w.harris@mac.com>
2020-02-19 14:19:19 +0000

    added rest days features

--- ncaa_predict/features/simple.py
+++ ncaa_predict/features/simple.py
@@ -1,8 +1,8 @@
 from . import registry
 from ..data.access import DataAccess
 from ..data.processed import regular_season_compact_team_results_df
-from ..utils import memoize
 import pandas as pd
+import numpy as np
 
 
 @registry.register
@@ -15,3 +15,26 @@ def score_difference(access: DataAccess) -> pd.Series:
 def win(access: DataAccess) -> pd.Series:
     ctr_df = regular_season_compact_team_results_df(access)
     return (ctr_df.Score > ctr_df.OtherScore).rename('Win')
+
+
+def _rest_days(access: DataAccess, maximum: int) -> pd.Series:
+    ctr_df = regular_season_compact_team_results_df(access).reset_index()
+    ctr_df['Day'] = (ctr_df.Season * 365) + ctr_df.DayNum
+    ctr_df['RestDays'] = ctr_df.groupby('TeamID').Day.diff().fillna(maximum)
+    ctr_df.RestDays = np.where(ctr_df.RestDays > maximum, maximum, ctr_df.RestDays)
+    return ctr_df['RestDays']
+
+
+@registry.register
+def rest_days_two_weeks_max(access: DataAccess):
+    return _rest_days(access=access, maximum=14)
+
+
+@registry.register
+def rest_days_one_week_max(access: DataAccess):
+    return _rest_days(access=access, maximum=7)
+
+
+@registry.register
+def rest_days_three_days_max(access: DataAccess):
+    return _rest_days(access=access, maximum=3)
