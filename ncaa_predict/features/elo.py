482f74f6f89cb2f4d9d16e20bc5e5782049b53fd
rich <richard.w.harris@mac.com>
2020-02-20 09:57:41 +0000

    Elo seems to work now.

--- ncaa_predict/features/elo.py
+++ ncaa_predict/features/elo.py
@@ -18,19 +18,31 @@ def elo(access: DataAccess) -> pd.DataFrame:
 
     team_conferences_df = access.team_conferences_df()
 
-    def _f(x):
-        return x
-
-    team_conferences_df.groupby('ConfAbbrev').apply(_f)
+    teams_by_season_and_conference = team_conferences_df.groupby('Season') \
+        .apply(lambda x: x.groupby('ConfAbbrev').TeamID.apply(list).to_dict()).to_dict()
 
     for season, season_df in _all_season_compact_results_df.groupby('Season'):
 
+        # We apply a regression to conference mean at the start of each season.
+        teams_by_conference = teams_by_season_and_conference.get(season, {})
+        conference_average_elo = {conference: sum(team_elo.get(team, r_0) for team in teams) / len(teams)
+                                  for conference, teams in teams_by_conference.items()}
+
+        for conference, teams in teams_by_conference.items():
+            average_elo = conference_average_elo.get(conference, r_0)
+            for team in teams:
+                team_elo[team] = 0.25 * average_elo + 0.75 * team_elo.get(team, r_0)
+
+        # Apply Elo to season games.
         for i, row in season_df.iterrows():
             w_loc = row.WLoc
-            r_w = team_elo.get(row.WTeamID, r_0) + (100 if w_loc == 'H' else 0)
-            r_l = team_elo.get(row.LTeamID, r_0) + (100 if w_loc == 'A' else 0)
+            r_w = team_elo.get(row.WTeamID, r_0)
+            r_l = team_elo.get(row.LTeamID, r_0)
 
-            elo_diff_w = r_w - r_l
+            r_w_ha = r_w + (100 if w_loc == 'H' else 0)
+            r_l_ha = r_l + (100 if w_loc == 'A' else 0)
+
+            elo_diff_w = r_w_ha - r_l_ha
             mov_w = row.WScore - row.LScore
 
             k = 20 * ((mov_w + 3) ** 0.8) / (7.5 + 0.006 * elo_diff_w)
@@ -38,8 +50,8 @@ def elo(access: DataAccess) -> pd.DataFrame:
             s_w = 1
             s_l = 0
 
-            e_w = 1 / (1 + 10 ** ((r_l - r_w) / 400))
-            e_l = 1 / (1 + 10 ** ((r_w - r_l) / 400))
+            e_w = 1 / (1 + 10 ** ((r_l_ha - r_w_ha) / 400))
+            e_l = 1 / (1 + 10 ** ((r_w_ha - r_l_ha) / 400))
 
             r_w_new = k * (s_w - e_w) + r_w
             r_l_new = k * (s_l - e_l) + r_l
@@ -57,4 +69,4 @@ def elo(access: DataAccess) -> pd.DataFrame:
     _all_season_compact_results_df['WEloAfter'] = w_elo_after
     _all_season_compact_results_df['LEloAfter'] = l_elo_after
 
-    return pd.DataFrame()
+    return _all_season_compact_results_df
